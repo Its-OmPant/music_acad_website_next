76a28f434c928b04ab6fe24e2ccbdb29b70fb01d
Om Pant <ompant992@gmail.com>
2025-08-21 18:24:03 +0530

    added Webinars Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 import Testimonials from "@/components/Testimonials";
 import { Spotlight } from "@/components/ui/spotlight-new";
-import { SP } from "next/dist/shared/lib/utils";
+import Webinars from "@/components/Webinars";
 
 export default function Home() {
 	return (
@@ -11,6 +11,7 @@ export default function Home() {
 			<HeroSection />
 			<FeaturedCourses />
 			<Testimonials />
+			<Webinars />
 			<div className="h-80"></div>
 		</main>
 	);
