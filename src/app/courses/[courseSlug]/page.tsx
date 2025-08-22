04d71f4adc88ba35295088a042b2ef021bf951a1
Om Pant <ompant992@gmail.com>
2025-08-23 01:19:09 +0530

    deploy: PageProps Type fix

--- src/app/courses/[courseSlug]/page.tsx
+++ src/app/courses/[courseSlug]/page.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import courses from "@/data/courses.json";
 import CoursePage from "@/components/CoursePage";
 
-function page({ params }) {
-	const { courseSlug } = params;
+type PageProps = {
+	params: {
+		courseSlug: string;
+	};
+	searchParams?: { [key: string]: string | string[] | undefined };
+};
 
+export default function Page({ params }: PageProps) {
+	const { courseSlug } = params;
 	return <CoursePage slug={courseSlug} />;
 }
-
-export default page;
 
 // necessary for static exports
 export async function generateStaticParams() {
